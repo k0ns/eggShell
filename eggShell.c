2ae2837353c748306e8172dd47c34a39a7b66b40
k0ns <23336711+k0ns@users.noreply.github.com>
2018-02-13 13:44:14 +0200

    Add files via upload

--- eggShell.c
+++ eggShell.c
@@ -4,15 +4,14 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-       
+
+//MAX ARRAY SIZES
 #define MAX_LINE_LENGTH 513
 #define MAX_ARG_LENGTH	8
 #define MAX_ARG_NO		16
 #define MAX_COM_NO		32
 #define MAX_COM_LENGTH	MAX_ARG_NO*MAX_ARG_LENGTH
 
-#define GREEN "\x1B[32m"
-#define RESET "\x1B[0m"
 
 void interactive();
 void batch(char **argv);
@@ -39,54 +38,66 @@ int main(int argc, char **argv){
 	
 }
 
-void interactive(){
 
+
+
+
+//COLOURS FOR THE PROMPT
+#define GREEN "\x1B[32m"
+#define RESET "\x1B[0m"
+
+void interactive(){
+	
+	//MALLOC LINE
 	char *line = (char *)malloc(MAX_LINE_LENGTH*sizeof(char));
 	if(!line){
 		printf("Allocation Error. Exitting...");
 		exit(1);
 	}
 	
-	int stop;	
 	
+	//PRINT PROMPT, READ LINE, EXECUTE COMMANDS IN LINE
+	//LOOP UNTIL QUIT RETURNED FROM EGG FUNCTION
+	int quit;	
 	do{
 		printf(GREEN "kokolakis_8415> " RESET);
 		fgets(line,MAX_LINE_LENGTH,stdin);
-		stop = egg(line);
-	}while(!stop);
+		quit = egg(line);
+	}while(!quit);
 	
 	free(line);
 }
 
 void batch(char **argv){
 
-	//OPEN BATCH FILE
-	FILE *fp;
-
-	if(!(fp = fopen(argv[1],"r"))){
-		printf("Error Opening File\nExiting...\n");
-		exit(1);
-	}
-	
 	char *line = (char *)malloc(MAX_LINE_LENGTH*sizeof(char));
 	if(!line){
 		printf("Allocation Error. Exitting...");
 		exit(1);
 	}
 	
-	int stop;	
-	
+	//OPEN BATCH FILE
+	FILE *fp;
+	if(!(fp = fopen(argv[1],"r"))){
+		printf("Error Opening File\nExiting...\n");
+		exit(1);
+	}
+
+	int quit;	
 	printf("\n\nSTARTING BATCH JOB\n\n");
+	//READ AND EXECUTE LINE BY LINE
+	//STOP WHEN NO MORE LINES OR QUIT RETURNED BY EGG
 	do{
 		if(fgets(line,MAX_LINE_LENGTH,fp) == NULL)		break;
-		stop = egg(line);
-	}while(!stop);
+		quit = egg(line);
+	}while(!quit);
 	printf("\n\nJOB FINISHED. GOOD BYE...\n\n");
 	
 	free(line);
 	fclose(fp);
 }
 
+//EXECUTE A SIGNLE COMMAND
 int exec(char **args){
 	
 	int status;
@@ -95,16 +106,16 @@ int exec(char **args){
 	
 	if(!(pid = fork())){
 		//CHILD
-		if(execvp(args[0],args)){
-			printf("Error. Could not execute command\n");
+		if(execvp(args[0],args) == -1){
 			exit(EXIT_FAILURE);
 		}
+		exit(EXIT_SUCCESS);
 	}
 	else if(pid > 0){
 		//PARENT
 		wait(&status);
-		if(WEXITSTATUS(status) == EXIT_FAILURE){
-			printf("Execution Failed\nExit Status: %d\n", WEXITSTATUS(status));
+		if(WIFEXITED(status) && WEXITSTATUS(status) == EXIT_FAILURE){
+			printf("Execution Failed\nContinuing...\n");
 			return EXIT_FAILURE;
 		}
 		else{
@@ -112,17 +123,20 @@ int exec(char **args){
 		}
 	}
 	else{
-		printf("Execution Failed.\nExiting...");
+		printf("Execution Failed.\nContinuing...\n");
 		return EXIT_FAILURE;
 	}
 }
 
+//DEFINE VALUES TO TELL DELIMITERS APART
 #define AMPER 1
 #define SEMI  2
+//INPUT = LINES
+//OUTPUT = COMMANDS, DELIM
 void parse_line(char *line,int *delim,char **commands){	
 	
 	char *dup = strdup(line);
-	//PARSE INPUT
+	
 	
 	commands[0] = strtok(line,";&");
 	
@@ -138,6 +152,7 @@ void parse_line(char *line,int *delim,char **commands){
 			*delim = 0;
 	}
 	
+	//CONTINUE PARSING
 	int it = 1;
 	do{
 		if((commands[it++] = strtok(NULL,";&")) == NULL)	break;
@@ -146,6 +161,7 @@ void parse_line(char *line,int *delim,char **commands){
 	free(dup);
 }
 
+//DEFINE VALUES FOR EMPTY COMMAND, OR 'QUIT' COMMAND
 #define QUIT 1
 #define EMPTY 2
 int parse_command(char *command, char **args){
@@ -153,13 +169,15 @@ int parse_command(char *command, char **args){
 	char *token;
 	
 	if(!strcmp(command,"quit\n")){
+		//CHECK FOR QUIT COMMAND
 		return QUIT;
 	}
 	else if(command[0] == '\n' || command[0] == '\0'){
+		//CHECK FOR EMPTY COMMAND
 		return EMPTY;
 	}
 	else{
-		//PARSE COMMAND
+		//ELSE PARSE COMMAND
 		token = strtok(command," \n");
 		int it = 0;
 		do{
@@ -173,7 +191,17 @@ int parse_command(char *command, char **args){
 	}
 }
 
+/*CENTRAL FUNCTION
 
+INPUT = LINE
+
+1. PARSE LINE
+2. PARSE COMMANDS IN LINE
+3. EXECUTE COMMANDS
+
+
+RETURN QUIT = 1 IF 'QUIT' COMMAND
+*/
 int egg(char *line){
 
 	char **commands = (char **)calloc(MAX_COM_NO,sizeof(char *));
@@ -188,8 +216,7 @@ int egg(char *line){
 	
 	parse_line(line,&delim,commands);
 	
-	//LOOP
-	
+	//EXECUTE COMMANDS IN LINE
 	int status;
 	int quit = 0;
 	int i = 0;
